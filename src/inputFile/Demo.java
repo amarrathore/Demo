efbbbe92034d429eeb5b784f5174df37403be0b7
amarrathore <bcs.amar1@gmail.com>
2017-03-29 17:48:15 +0530

    updated

--- src/inputFile/Demo.java
+++ src/inputFile/Demo.java
@@ -1,6 +1,6 @@
 package inputFile;
 
-public class Demo {	
+public class Demo {
 	public static void main(String[] args) throws Exception {
 		SeleniumUtilities.updateResult("hello", "ModuleName", true, "Website");
 	}
