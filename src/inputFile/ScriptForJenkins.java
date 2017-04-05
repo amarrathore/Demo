5b44eb510f1bf5823add8351a4aea48cfa9bf6d3
amarnath.rathore <amarnath.rathore@bcslappy-Amar.BCSTECH.IND>
2017-04-05 18:22:50 +0530

    updated

--- src/inputFile/ScriptForJenkins.java
+++ src/inputFile/ScriptForJenkins.java
@@ -10,38 +10,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ScriptForJenkins {
 	public WebDriver driver;
-	
-	  @Test
-	  public void test() {
-		  System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
-		driver = new FirefoxDriver();
-	    driver.get("https://www.volaris.com/?culture=en-US&Flag=us");	    
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    
-	    try {
-			driver.findElement(By.name("opc")).click();
-			
-			String parent = driver.getWindowHandle();			
-			Set<String> set = driver.getWindowHandles();
-			Iterator<String> iterator = set.iterator();
-			while(iterator.hasNext()) {
-			   String childWindow = iterator.next();
-			   if(!parent.equals(childWindow)) {
-				   driver.switchTo().window(childWindow);
-			   }				   
-			driver.findElement(By.xpath("//*[@id='input_origin']")).click();
-			driver.findElement(By.xpath("//*[@id='btn_origen']")).click();
-			driver.findElement(By.linkText("Acapulco")).click();
-			//driver.findElement(By.xpath("//*[@id='MX']/div/ul[1]/li[1]/a")).click();
-			driver.findElement(By.xpath("//*[@id='input_destination']")).click();
-			driver.findElement(By.xpath("//*[@id='btn_origen']")).click();
-			//driver.findElement(By.linkText("Mexico City")).click();
-			driver.findElement(By.xpath("//*[@id='MX']/div/ul[1]/li[1]/a")).click();
-						
-  }
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
+		
+	@Test
+	public void test() {
+	System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
+	driver = new FirefoxDriver();
+	driver.get("https://www.volaris.com/?culture=en-US&Flag=us");	    
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	try {
+		driver.findElement(By.name("opc")).click();
+		String parent = driver.getWindowHandle();			
+		Set<String> set = driver.getWindowHandles();
+		Iterator<String> iterator = set.iterator();
+		while(iterator.hasNext()) {
+		   String childWindow = iterator.next();
+		   if(!parent.equals(childWindow)) {
+			   driver.switchTo().window(childWindow);
+		   }
 		}
-	  } 
+		//driver.findElement(By.xpath("//*[@id='input_origin']")).click();
+		driver.findElement(By.xpath("//*[@id='btn_origen']")).click();
+		driver.findElement(By.linkText("Acapulco")).click();
+		//driver.findElement(By.xpath("//*[@id='MX']/div/ul[1]/li[1]/a")).click();
+		//driver.findElement(By.xpath("//*[@id='btn_origen']")).click();
+		driver.findElement(By.xpath("//*[@id='input_destination']")).click();
+		driver.findElement(By.xpath("//*[@id='liCity1']/a")).click();		
+		driver.findElement(By.linkText("Oakland (San Francisco area)")).click();
+		//driver.findElement(By.linkText("Mexico City")).click();
+		//driver.findElement(By.xpath("//*[@id='MX']/div/ul[3]/li[6]/a")).click();
+	} catch (Exception e) {
+		System.out.println(e.getMessage());
+		e.printStackTrace();
+	}
+	}
 }
