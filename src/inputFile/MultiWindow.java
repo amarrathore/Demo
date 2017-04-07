3953ac8a2bb891819181864cc073f8ef0aa505dc
amarnath.rathore <amarnath.rathore@bcslappy-Amar.BCSTECH.IND>
2017-04-07 11:54:55 +0530

    updated

--- src/inputFile/MultiWindow.java
+++ src/inputFile/MultiWindow.java
@@ -14,11 +14,8 @@ public class MultiWindow {
 public void TestPopUp() throws InterruptedException{
 // Open browser
 WebDriver driver=new FirefoxDriver();
-
 // Maximize browser
 driver.manage().window().maximize();
-
-// Load app
 driver.get("http://www.naukri.com/");
 
 // It will return the parent window name as a String
@@ -31,14 +28,9 @@ while(iterator.hasNext()) {
    if(!parent.equals(childWindow)) {
 	   driver.switchTo().window(childWindow);
    }
-System.out.println(driver.switchTo().window(childWindow).getTitle());
-}
-
-driver.close();
-}
-
+	System.out.println(driver.switchTo().window(childWindow).getTitle());
+	}
 // once all pop up closed now switch to parent window
 driver.switchTo().window(parent);
-
 }
 }
--- src/inputFile/WindowHandle.java
+++ src/inputFile/WindowHandle.java
@@ -2,23 +2,20 @@ package inputFile;
 
 import java.util.Iterator;
 
-import org.openqa.selenium.By;
+import java.util.Set;
 import org.openqa.selenium.WebDriver;
-
-import com.gargoylesoftware.htmlunit.javascript.host.Set;
 
 public class WindowHandle {
 	public WebDriver driver;
 	public void window() {
-	 String windowHandle = driver.getWindowHandle();
-	    driver.findElement(By.name("opc")).click();
-	    Set<String> handles =  driver.getWindowHandles();
-	    Iterator<String> iterator = handles.Iterator();
+		String parentWindow = driver.getWindowHandle();	    
+	    Set<String> set =  driver.getWindowHandles();
+	    Iterator<String> iterator = set.iterator();
 	      while(iterator.hasNext()) {
-	    	  String handle = iterator.next();
-	           if(!windowHandle.equals(handle)) {
-	              driver.switchTo().window(handle).getTitle();
-	              }
-	       }
+	    	  String childWindow = iterator.next();
+	    	  if(!parentWindow.equals(childWindow)) {
+	        	  driver.switchTo().window(childWindow).getTitle();
+	        	  }
+	      }
 	}
 }
