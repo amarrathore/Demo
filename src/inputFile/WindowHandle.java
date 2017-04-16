15eac70facc9465e913478e154e3ffd998dfd833
amarnath.rathore <amarnath.rathore@bcslappy-Amar.BCSTECH.IND>
2017-04-16 11:29:48 +0530

    updated

--- src/inputFile/WindowHandle.java
+++ src/inputFile/WindowHandle.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 import java.util.Set;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
 public class WindowHandle {
 	public WebDriver driver;
@@ -17,5 +18,24 @@ public class WindowHandle {
 	        	  driver.switchTo().window(childWindow).getTitle();
 	        	  }
 	      }
+	}
+	
+
+	
+	@Test(enabled = false)
+	public void switchTab() {
+		try {
+			String parentWindow = driver.getWindowHandle();	    
+			Set<String> set =  driver.getWindowHandles();
+			Iterator<String> iterator = set.iterator();
+			while(iterator.hasNext()) {
+				String childWindow = iterator.next();
+				if(!parentWindow.equals(childWindow)) {
+					driver.switchTo().window(childWindow).getTitle();
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
